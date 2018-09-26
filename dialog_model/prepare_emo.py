70cc0854dab41e9f7f0ba11fab6ebc718b6c3c65
Asai Sara <sara-as@ahcpascal01.naist.jp>
2018-09-26 17:11:38 +0900

    emotionラベルの読み込み

---
+++ dialog_model/prepare_emo.py
@@ -0,0 +1,21 @@
+emo2index = {"平静":0,"怒り":1,"悲しみ":2, "喜び":3, "安心":4}
+index2emo = {"平静","怒り","悲しみ","喜び",
+
+#ロボット側の感情ラベルの読み込み
+emotions = open('cleaning/robot_emotions.txt', encoding='utf-8').read().strip().split('\n')
+emo_index = [[emo2index[e]] for e in emotions]
+emo_tensor = torch.tensor(emo_index, dtype=torch.long, device=device)
+
+#下の関数は今は使わない、入力から感情を予測する場合に使う
+class PredictEmotion(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(PredictEmotion, self).__init__()
+        self.li1 = nn.Linear(hidden_size, hidden_size)
+        self.li2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h = self.li1(x)
+        h = F.tanh(h)
+        y = self.li2(h)
+        y = F.log_softmax(y,dim=1)
+        return y
