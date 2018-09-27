53f2dcb0a709c5b4d82a60f5e23f9b22046309a6
Asai Sara <sara-as@ahcpascal01.naist.jp>
2018-09-28 01:14:17 +0900

    emotionの処理はload_filesで行うことに変更

--- dialog_model/prepare_emo.py
+++ dialog_model/prepare_emo.py
@@ -1,12 +1,15 @@
+######## 他のファイルに移したので使わない######
+
+
 emo2index = {"平静":0,"怒り":1,"悲しみ":2, "喜び":3, "安心":4}
-index2emo = {"平静","怒り","悲しみ","喜び",
+index2emo = {"平静","怒り","悲しみ","喜び","安心"}
 
 #ロボット側の感情ラベルの読み込み
 emotions = open('cleaning/robot_emotions.txt', encoding='utf-8').read().strip().split('\n')
 emo_index = [[emo2index[e]] for e in emotions]
 emo_tensor = torch.tensor(emo_index, dtype=torch.long, device=device)
 
-#下の関数は今は使わない、入力から感情を予測する場合に使う
+#下のクラスは今は使わない、入力から感情を予測する場合に使う
 class PredictEmotion(nn.Module):
     def __init__(self, hidden_size, output_size):
         super(PredictEmotion, self).__init__()
