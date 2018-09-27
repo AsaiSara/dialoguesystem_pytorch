9f13f86525786beecca5bae9cfa82e153ac87ed0
Asai Sara <sara-as@ahcpascal01.naist.jp>
2018-09-28 01:14:43 +0900

    emotion処理追加

--- dialog_model/load_files.py
+++ dialog_model/load_files.py
@@ -34,12 +34,11 @@ class Lang:
     def normalizeString(self,s):
         return s.strip()
 
-
-def readLangs(lang1,lang2,reverse=False):
+def readLangs(lang1,lang2,domain,reverse=False):
     print("Reading lines...")
 
     #Read the file and split into lines
-    lines = open("data/cleaning/%s-%s.txt" % (lang1,lang2), encoding='utf-8').read().strip().split('\n')
+    lines = open("data/%s/%s-%s.txt" % (domain,lang1,lang2), encoding='utf-8').read().strip().split('\n')
 
     #Split every line into pairs and normalize
     pairs = [[ s.strip() for s in l.split('\t')] for l in lines]
@@ -52,7 +51,6 @@ def readLangs(lang1,lang2,reverse=False):
     else:
         input_lang = Lang(lang1)
         output_lang = Lang(lang2)
-        
     return input_lang, output_lang, pairs
 
 #Filtering Data    
@@ -84,31 +82,45 @@ def tensorsFromPair(pair):
 
 
 #Load Data Corpus 
-def prepareData(lang1, lang2, reverse=False):
-    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+def prepareData(lang1, lang2, domain , reverse=False):
+    #reverseにすると入力文と出力文が逆になる
+    #lang1はinput文の名前,lang2はoutput文の名前,domainは対話のドメイン名
+    #lang1 = 'hu' #human
+    #lang2 = 'ro' #robot
+    #domain = 'cleaning' #file読み込み時のdirectry指定に使うだけ
+    #input文のLangクラス,output文のLangクラス, 発話対の配列を返す
+    #この時点ではクラスに名前とreverseの有無のみ入っている
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, domain, reverse)
     print("Read %s sentence pairs" % len(pairs))
+    #発話対のデータをフィルターにかける
     pairs = filterPairs(pairs)
     print("Trimmed to %s sentence pairs" % len(pairs))
     print("Couonted words...")
+    #発話対に含まれる単語をクラス内の辞書に追加していく
     for pair in pairs:
         input_lang.addSentence(pair[0])
         output_lang.addSentence(pair[1])
     print("Counted words:")
     print(input_lang.name, input_lang.n_words)
     print(output_lang.name, output_lang.n_words)
+    #input文のクラス(辞書),output文のクラス(辞書)を返す
     return input_lang, output_lang, pair
 
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+class Emo(self, name):
+    self.name = name
+    self.emo2index = {"平静":0,"怒り":1,"悲しみ":2, 
+                        "喜び":3, "安心":4}
+    self.index2emo = {"平静","怒り","悲しみ","喜び","安心"}
+
+#Load Robot_Emotion 
+def LoadEmo(domain,lang3):
+    #lang3= robot or human
+    emo = Emo(lang3)
+    emotions = open('%s/%s_emotions.txt' % (domain,lang3), encoding='utf-8').
+        read().strip()split('\n')
+    emo_id = [[emo.emo2index[e] for e in emotions]
+    emo_tensor = torch.tensore(emo_id, dtype=torch.long, device=decice)
+    print("Read %s %s-emotions" % len(emotions))
+    return emo_tensor
 
