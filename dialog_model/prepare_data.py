c8470645d643aa662460a29ac253cfc56ced1531
Asai Sara <sara-as@ahcpascal01.naist.jp>
2018-09-29 23:31:02 +0900

    add load data function

--- dialog_model/prepare_data.py
+++ dialog_model/prepare_data.py
@@ -1,49 +1,13 @@
-class Prepare_data(Lang):
-    MAX_LENGTH = 80
-    def readLangs(self,lang1,lang2,reverse=False):
-        print("Reading lines...")
-        
-        #Read the file and split into lines
-        lines = open("data/cleaning/%s-%s.txt" % (lang1,lang2), encoding='utf-8').read().strip().split('\n')
+from load_files import *
 
-        #Split every line into pairs and normalize
-        pairs = [[ s for s in l.split('\t')] for l in lines]
-
-        #Reverse pairs, make Lnaf instance
-        if reverse:
-            pairs = [list(reverse(p)) for p in pairs]
-            input_lang = Lang(lang2)
-            output_lang = Lang(lang1)
-        else:
-            input_lang = Lang(lang1)
-            output_lang = Lang(lang2)
-
-        return input_lang, output_lang, pairs
-
-    def filterPair(self,p):
-        return len(p[0].split(' ')) < MAX_LENGTH and len([1].split(' ')) < MAX_LENGTH
+def SetData(input_name, output_name, domain_name, emo_name,dev_num,train_num):
     
-    def filterPairs(self,pairs):
-        return [pair for pair in pairs if filterPair(pair)]
+    input_lang, output_lang, pair = prepareData(input_name,output_name,domain_name)
+    #test_numはtestとdevの残り
+    train_pairs = pair[:train_num]
+    dev_pairs = pair[train_num:train_num + dev_num]
+    test_pairs = pair[train_num + dev_num :]
 
-    def prepareData(self,input_lang, output_lang, pairs, reverse=False):
-    #input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
-        print("Read %s sentence pairs" % len(pairs))
-        pairs = filterPair(pairs)
-        print("Trimmed to %s sentence pairs" % len(pairs))
-        print("Counted words...")
-        for pair in pairs:
-            input_lang.addSentence(pair[0])
-            output_lang.addSentence(pair[1])
-        print("counted words:")
-        print(input_lang.name, input_lang.nn_words)
-        print(output_lang.name, output_lang.n_words)
-        return input_lang, output_lang, pairs
-"""    	
-input_lang, output_lang, pairs = prepareData('hu', 'ro')
+    emo_tensor = LoadEmo(domain_name,emo_name)
+    return input_lang, output_lang, train_pairs, dev_pairs, test_pairs, emo_tensor
 
-dev_pairs = pairs[1200:1350]
-test_pairs = pairs[1350:]
-pairs = pairs[:900]
-print(rancom.choice(pairs))
-"""
